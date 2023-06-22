4777717d7c782268d0d4f40b4cd88533c1ebf57c
GilermeDC <guilhermecortiivo@gmail.com>
2023-06-22 14:58:04 -0300

    finalizando initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  /*substituindo a foto */
+  /* pegando a tag img */
+  const img = document.querySelector("#profile img")
+  /* substituindo a imagem*/
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light//
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver fora do light mode, adicionar a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
